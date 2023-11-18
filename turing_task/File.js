1009b1cf4056e7c79a32743f065eca4cc804cbed
adeelmanaf43 <adeelmanaf00@gmail.com>
2023-11-18 16:56:41 +0500

    React is almost completed

---
+++ turing_task/File.js
@@ -0,0 +1,36 @@
+// var calPoints = function (ops: any) {
+//   var result = null;
+//   return result;
+// };
+var calPoints = function (ops) {
+    let result = [];
+    for (let i = 0; i < ops.length; i++) {
+        if (Number(ops[i])) {
+            result.push(Number(ops[i]));
+        }
+        else if (ops[i] === "C") {
+            result.pop();
+        }
+        else if (ops[i] === "D") {
+            result[result.length] = result[result.length - 1] * 2;
+        }
+        else if (ops[i] === "+") {
+            let lastIndex = result.length;
+            let a = result[lastIndex - 1];
+            let b = result[lastIndex - 2];
+            result.push(a + b);
+        }
+    }
+    let finalResult;
+    if (result.length > 0) {
+        finalResult = result.reduce((sum, a) => sum + a);
+    }
+    else {
+        finalResult = 0;
+    }
+    return finalResult;
+};
+console.log(calPoints(["1", "C"]));
+console.log(calPoints(["5", "2", "C", "D", "+"]));
+console.log(calPoints(["5", "-2", "4", "C", "D", "9", "+", "+"]));
+export {};
---
+++ turing_task/Merge.js
@@ -0,0 +1,43 @@
+// let a = [1];
+// let b = [0];
+// var merge = function (nums1: number[], nums2: number[]) {
+//   //   nums1 = nums1.filter((item) => item !== 0);
+//   //   nums2 = nums2.filter((item) => item !== 0);
+//   for (let i = 0; i < nums1.length; i++) {
+//     if (nums1[i] === 0) {
+//       delete nums1[i];
+//     }
+//   }
+//   for (let i = 0; i < nums2.length; i++) {
+//     if (nums2[i] === 0) {
+//       delete nums2[i];
+//     }
+//   }
+//   let first;
+//   let second;
+//   let max_a = Math.max(...nums1);
+//   let max_b = Math.max(...nums2);
+//   if (max_a > max_b) {
+//     first = nums1;
+//     second = nums2;
+//   } else {
+//     second = nums1;
+//     first = nums2;
+//   }
+//   for (let i = 0; i < first.length; i++) {
+//     for (let j = 0; j < second.length; j++) {
+//       if (second[j] <= first[i]) {
+//         first.splice(i, 0, second[j]);
+//         delete second[j];
+//         break;
+//       }
+//     }
+//   }
+//   nums1 = first;
+//   return nums1;
+// };
+// console.log(merge([1, 2, 3, 0, 0, 0], [2, 5, 6]));
+var removeElement = function (nums, val) {
+    return nums.filter(function (item) { return item !== val; });
+};
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
